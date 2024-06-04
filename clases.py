1e19ab7ff3ac28a6ed03e49d9083a69906d16f5d
carmenarc <166650946+carmenarc@users.noreply.github.com>
2024-06-04 20:35:19 +0200

    Update clases.py

--- clases.py
+++ clases.py
@@ -80,19 +80,4 @@ class Empleado:
     def calcular_salario_mensual(self):
         return self._salario_anual / 12
  
-class Gerente(Empleado):
-    def __init__(self, nombre, edad, salario_anual, bono):
-        super().__init__(nombre, edad, salario_anual)
-        self._bono = bono
- 
-    @property
-    def bono(self):
-        return self._bono
- 
-    @bono.setter
-    def bono(self, nuevo_bono):
-        self._bono = nuevo_bono
- 
-    def calcular_salario_mensual(self):
-        salario_base_mensual = super().calcular_salario_mensual()
-        return salario_base_mensual + self._bono / 12
+
